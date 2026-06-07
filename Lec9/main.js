15a229f746ee59d97c7af04cbe37b9789ed9e9ca
Nikoiso <nick.chubinidzee@gmail.com>
2026-06-07 06:32:51 -0700

    Gasworebuli Davaleba

--- Lec9/main.js
+++ Lec9/main.js
@@ -33,22 +33,22 @@
 
 // 3) დაწერე ფუნქიცა რომელიც დააბრუნებს true თუ რიცხვი მარტივია თუ არადა false.
 
-// let num = 2
+let num = 2
 
-// function trueorFalse(num) {
-//   if (num <= 1) {
-//     return false
-//   }
+function trueorFalse(num) {
+  if (num <= 1) {
+    return false
+  }
 
-//   for (i = 2; i < num; i++) {
-//     if (num % i === 0) {
-//       return false
-//     }
-//   }
-//   return true
-// }
+  for ( let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false
+    }
+  }
+  return true
+}
 
-// console.log(trueorFalse(num))
+console.log(trueorFalse(num))
 
 // 4) let words = ["dog", "elephant", "cat", "hippopotamus"] იპოვე ყველაზე გრძელი ისტყვა
 
@@ -62,35 +62,58 @@
 
 // 5)let arr = [3, 5, 3, 2, 5, 5, 3, 5] დააბრუნე ისეთი რიცხვი რომელიც მეორდება უფრო მეტჯერ
 
-// let arr = [3, 5, 3, 2, 5, 5, 3, 5]
+// let arr = [3, 5, 3, 2, 5, 5, 3, 5];
 
-// let doubleNum = arr.filter
+// let mostRepeated = arr[0];
+// let maxCount = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//   let num = arr[i];
+//   let count = 0;
+
+//   for (let j = 0; j < arr.length; j++) {
+//     if (arr[j] === num) {
+//       count++;
+//     }
+//   }
+
+//   if (count > maxCount) {
+//     maxCount = count;
+//     mostRepeated = num;
+//   }
+// }
+
+// console.log(mostRepeated);
 
 // 6)let nums = [1, 2, 3, 4, 5, 6, 7, 8] დაწერე ფუქნცია რომელიც დაითვლის რამდენი ლუწი და რამდენი კენტი რიცხვია
 
 // let nums = [1, 2, 3, 4, 5, 6, 7, 8];
 
-// function numbers(arr) {
-//   nums.forEach((num) => {
-//     if (num % 2 === 0) {
-//       console.log(`${num} Aris Luwi`);
-//     } else {
-//       console.log(`${num} Aris Kenti`);
+// function evenOrOdd(nums){
+//     let evenCount = 0
+//     let oddCount = 0
+
+//     for(let i = 0; i < nums.length; i++){
+//         if (nums[i] % 2 === 0){
+//             evenCount++
+//         }else{
+//             oddCount++
+//         }
 //     }
-//   });
+//     console.log(`Luwi: ${evenCount}`)
+//     console.log(`Kenti: ${oddCount}`)
 // }
 
-// numbers(nums);
+// evenOrOdd(nums)
 
 // 7)let nums = [10, 2, 33, 5, 7] დაწერე ფუქნცია როემლიც დააბრუენბს ყველაზე პატარა რიცხვს
 
-let nums = [10, 2, 33, 5, 7]
+// let nums = [10, 2, 33, 5, 7];
 
-function sortedArr(arr){
-    arr.sort((a,b) => a-b)
+// function sortedArr(arr) {
+//   arr.sort((a, b) => a - b);
 
-    return nums[0]
+//   return nums[0];
+// }
 
-}
-
-console.log(sortedArr(nums))
+// console.log(sortedArr(nums));
